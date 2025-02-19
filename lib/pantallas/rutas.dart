dbc4b43cd27e06a5c52359e701185c82bdf7d069
DanielGiron20 <girondaniel388@gmail.com>
2025-02-19 15:13:30 -0600

    json

--- lib/pantallas/rutas.dart
+++ lib/pantallas/rutas.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:tradeunitec/pantallas/login.dart';
-import 'package:tradeunitec/pantallas/logon.dart';
 import 'package:tradeunitec/pantallas/pantalla_principal.dart';
+import 'package:tradeunitec/pantallas/logon.dart';
 
 enum MyRoutes {
   // ignore: constant_identifier_names
   PantallaPrincipal,
   Login,
-  Logon
+  Logon,
 }
 
 final Map<String, Widget Function(BuildContext)> routes = {
   MyRoutes.PantallaPrincipal.name: (context) => const PantallaPrincipal(),
-  MyRoutes.Login.name: (context) => Login(),
-  MyRoutes.Logon.name: (context) => Logon(),
+  MyRoutes.Login.name: (context) =>  Login(), 
+MyRoutes.Logon.name: (context) =>  Logon(),
+  
 };
